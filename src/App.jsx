4487feac0a13479a4ed10b8f350e7049ac048de0
p1gnat <hellwowo1@gmail.com>
2024-11-12 22:59:02 +0500

    just test to mount

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,10 @@ const App = () => {
   useEffect(() => {
     ApiSelector().connect();
     return () => {
+      console.log("Cleanup: disconnecting...");
       ApiSelector().disconnect();
     };
-  });
+  }, []);
   return (
     <>
       <h1>Welcome to Connection</h1>
